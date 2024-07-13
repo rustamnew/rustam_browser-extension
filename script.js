98db822490de252b50baadf73223104ad041b4d9
rustamnew <36161445+rustamnew@users.noreply.github.com>
2024-07-13 04:07:29 +0300

    1.03
     - Добавлен МегаМаркет
     - Добавлен поиск по всем маркетплейсам
    - Сохранение состояния полей
     - Улучшен код

--- script.js
+++ script.js
@@ -1,60 +1,119 @@
-const submitButtons = document.querySelectorAll('.submit-button')
+// Восстановление полей
+loadInputStates()
 
-submitButtons.forEach( (item) => {
-    item.addEventListener('click', function(e) {
-        const marketplaceName = item.dataset.marketplace //название маркетплейса
-        const itemInput = document.querySelector(`input[data-marketplace="${marketplaceName}"]`)
-        const itemValue = itemInput.value
+
+
+// Функционал открытия вкладки путём субмита <form>
+const forms = document.querySelectorAll('form.single')
+
+forms.forEach( (form) => {
+    form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+
+        await saveInputStates();
+
+        const URL_SEARCH = getPageFromForm(form)
     
-        if (!itemValue) {
-            return false
-        }
-        
-        
-        let searchString = '' //составной URL строки поиска
-        let BASE_URL = '' //URL страницы маркетплейса 
-        let WORD_DIVIDE = '' //("пробел" в URL запросе, иногда различается)
-
-
-        //Различные параметры в зависимости от паркетплейса
-        if (marketplaceName === 'ozon') {
-            BASE_URL = item.getAttribute("href") + `/search/?from_global=true&text=`
-            WORD_DIVIDE = '%20' 
-        }
-        if (marketplaceName === 'wb') {
-            BASE_URL = item.getAttribute("href") + `/catalog/0/search.aspx?search=`
-            WORD_DIVIDE = '%20'
-        }
-        if (marketplaceName === 'ym') {
-            BASE_URL = item.getAttribute("href") + `/search?text=`
-            WORD_DIVIDE = '+'
-        }
-        
-
-        //Проверка на количество слов > 1 (присутствует пробел)
-        if (itemValue.includes(' ')) { 
-            const itemValueArray = itemValue.split(' ')
-    
-            for (let i = 0; i < itemValueArray.length; i++) {
-                if (i < itemValueArray.length - 1) {
-                    searchString += itemValueArray[i] + WORD_DIVIDE 
-                }
-                else {
-                    searchString += itemValueArray[i]
-                }
-            }
-        } 
-        else {
-            searchString = itemValue
-        }
-    
-
         //Открыть новую вкладку с поиском по тексту из инпута
         chrome.tabs.create({
-            url: BASE_URL + searchString, 
+            url: URL_SEARCH, 
             active: true,
         });
         
         return false;
     })
 })
+
+
+
+// Поиск по всем маркетплейсам
+const formGlobal = document.querySelector('.form.global')
+
+formGlobal.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formInput = formGlobal.querySelector(`input`)
+    const formValue = formInput.value
+
+    const URL_ARRAY = []
+
+    forms.forEach( (form) => {
+        form.querySelector('input').value = formValue
+        const URL_SEARCH = getPageFromForm(form)
+        URL_ARRAY.push(URL_SEARCH)
+    })
+
+    await openLinks(URL_ARRAY);
+})
+
+
+
+// Составить URL с запросом поиска из элемента <form>
+function getPageFromForm(form) {
+    const formValue = form.querySelector(`input`).value
+    
+    if (!formValue) {
+        return false
+    }
+    
+    const BASE_URL = form.dataset.href + form.dataset.base //URL страницы маркетплейса (поиск)
+    let searchString = ''
+
+    //Проверка на количество слов > 1 (присутствует пробел)
+    if (formValue.includes(' ')) { 
+        const WORD_DIVIDE = form.dataset.divider //(пробел)
+        const formValueArray = formValue.split(' ')
+
+        for (let i = 0; i < formValueArray.length; i++) {
+            if (i < formValueArray.length - 1) {
+                searchString += formValueArray[i] + WORD_DIVIDE 
+            }
+            else {
+                searchString += formValueArray[i]
+            }
+        }
+    } 
+    else {
+        searchString = formValue
+    }
+
+    return BASE_URL + searchString
+}
+
+
+//Асинхнонное открытие ссылок из массива
+async function openLinks(url_array) {
+    await saveInputStates()
+
+    for (let i = 0; i < url_array.length; i++) {
+        chrome.tabs.create({
+            url: url_array[i], 
+            active: (i === url_array.length - 1) ? true : false, //Переключать на последнюю из открытых вкладок 
+        });
+    }
+}
+
+// Сохранение в localstorage
+async function saveInputStates() {
+    const values = []
+    const inputs = document.querySelectorAll('input')
+    inputs.forEach((input) => {
+        values.push(input.value)
+    })
+
+    
+    await chrome.storage.local.set({ // writing
+        values: values,
+    });
+}
+
+// Загрузка из localstorage
+async function loadInputStates() {
+
+    const values = await chrome.storage.local.get('values'); //reading
+    const inputs = document.querySelectorAll('input');
+
+    inputs.forEach((input, index) => {
+        input.value = values.values[index]
+    })
+}
